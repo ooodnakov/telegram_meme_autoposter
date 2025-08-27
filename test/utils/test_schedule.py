c3f137c38d882807d20cbf6a2f0181d735d66a7e
Aleksandr Odnakov <40039040+ooodnakov@users.noreply.github.com>
2025-08-27 12:59:11 +0300

    Add ids to schedule tests

--- test/utils/test_schedule.py
+++ test/utils/test_schedule.py
@@ -10,14 +10,15 @@ from telegram_auto_poster.utils.scheduler import (
 @pytest.mark.parametrize(
     "now, scheduled, quiet_start, quiet_end, expected",
     [
-        (
+        pytest.param(
             datetime.datetime(2024, 1, 1, 21, 30, tzinfo=datetime.timezone.utc),
             [],
             22,
             10,
             datetime.datetime(2024, 1, 2, 10, 0, tzinfo=datetime.timezone.utc),
+            id="after_night_empty_schedule",
         ),
-        (
+        pytest.param(
             datetime.datetime(2024, 1, 1, 21, 30, tzinfo=datetime.timezone.utc),
             [
                 (
@@ -30,8 +31,9 @@ from telegram_auto_poster.utils.scheduler import (
             22,
             10,
             datetime.datetime(2024, 1, 2, 11, 0, tzinfo=datetime.timezone.utc),
+            id="skip_one_occupied_slot",
         ),
-        (
+        pytest.param(
             datetime.datetime(2024, 1, 1, 21, 30, tzinfo=datetime.timezone.utc),
             [
                 (
@@ -50,13 +52,15 @@ from telegram_auto_poster.utils.scheduler import (
             22,
             10,
             datetime.datetime(2024, 1, 2, 12, 0, tzinfo=datetime.timezone.utc),
+            id="skip_multiple_occupied_slots",
         ),
-        (
+        pytest.param(
             datetime.datetime(2024, 1, 1, 5, 30, tzinfo=datetime.timezone.utc),
             [],
             2,
             6,
             datetime.datetime(2024, 1, 1, 6, 0, tzinfo=datetime.timezone.utc),
+            id="custom_quiet_hours",
         ),
     ],
 )
@@ -70,10 +74,10 @@ def test_find_next_slot(now, scheduled, quiet_start, quiet_end, expected):
 @pytest.mark.parametrize(
     "hour, quiet_start, quiet_end, expected",
     [
-        (3, 2, 6, True),
-        (7, 2, 6, False),
-        (23, 22, 10, True),
-        (12, 22, 10, False),
+        pytest.param(3, 2, 6, True, id="in_quiet_hours_day"),
+        pytest.param(7, 2, 6, False, id="outside_quiet_hours_day"),
+        pytest.param(23, 22, 10, True, id="in_quiet_hours_overnight_start"),
+        pytest.param(12, 22, 10, False, id="outside_quiet_hours_overnight"),
     ],
 )
 def test_in_quiet_hours(hour, quiet_start, quiet_end, expected):
